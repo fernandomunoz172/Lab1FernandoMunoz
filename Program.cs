2be0c8b5d7315057c4ed43b129485f0c33cfc4e0
Fernando Mu?oz <fernando.muoz@snow.edu>
2022-09-08 17:23:56 -0600

    Math Operations

--- Program.cs
+++ Program.cs
@@ -1,4 +1,90 @@
-﻿// See https://aka.ms/new-console-template for more information
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, User my name is Fernando , I will calculate your Generate Point Average. First of all, tell me your name: ");
-name= Console.ReadLine();
+﻿Console.WriteLine("MATH OPERATIONS");
+Console.WriteLine("------------------------------------------------------------------------------");
+Console.WriteLine("------------------------------------------------------------------------------");
+
+Console.WriteLine("ADITION");
+int a = 35;
+int b = 50;
+int c = 6;
+int result= (a + b);
+int result2 = result + c;
+Console.WriteLine($"The addition between {a} and {b} is: {result}");
+Console.WriteLine($"Which if we add {c} gives a result of {result2}");
+Console.WriteLine("------------------------------------------------------------------------------");
+
+Console.WriteLine("SUSTRACTION");
+int d = 100;
+int e = 59;
+int f = 50;
+int bresult= (d - e);
+int bresult2 = bresult - f;
+Console.WriteLine($"The subtraction between {d} and {e} is: {bresult}");
+Console.WriteLine($"And if to that result we subtract it {f} gives a result of {bresult2}");
+Console.WriteLine("------------------------------------------------------------------------------");
+
+Console.WriteLine("MULTIPLICATION");
+int g = 10;
+int h = 32;
+int cresult= (g*h);
+Console.WriteLine($"The product between {g} and {h} is: {cresult}");
+Console.WriteLine("------------------------------------------------------------------------------");
+
+Console.WriteLine("DIVISION");
+int i = 64;
+int j = 4;
+int dresult= (i/j);
+Console.WriteLine($"If we divide {i} and {j} we obtain: {dresult}");
+Console.WriteLine("------------------------------------------------------------------------------");
+
+Console.WriteLine("DIVISION WITH REMAINDERS");
+int k = 100;
+int l = 30;
+int eeresult=k/l;
+int eresult= (k%l);
+Console.WriteLine($"If we divide {k} and {l} we obtain: {eeresult} with a remainder of {eresult}");
+Console.WriteLine("------------------------------------------------------------------------------");
+
+Console.WriteLine("COMBINED OPERATIONS");
+int m = 90;
+int n= 38;
+int o= 4;
+int fresult=(m+n)/4;
+Console.WriteLine($"The addition bewteen {m} and {n} divided  by {o} is: {fresult}");
+Console.WriteLine("------------------------------------------------------------------------------");
+
+Console.WriteLine("DECIMAL ANSWERS");
+int u = 65;
+int v = 4;
+float gresult= (u/v);
+Console.WriteLine($"If we divide {u} and {v} we obtain: {gresult}");
+Console.WriteLine("------------------------------------------------------------------------------");
+
+Console.WriteLine("DECIMAL OPERATIONS");
+float w = 15_8;
+int x = 4;
+float hresult= (w/x);
+Console.WriteLine($"If we divide {w} and {x} we obtain: {hresult}");
+Console.WriteLine("------------------------------------------------------------------------------");
+/*
+WorkWithIntegers();
+
+void WorkWithIntegers()
+{
+    int aa = 27;
+    int bb = 6;
+    int cc = aa + bb;
+    Console.WriteLine(cc);
+
+
+    // subtraction
+    cc = aa - bb;
+    Console.WriteLine(cc);
+
+    // multiplication
+    cc = aa * bb;
+    Console.WriteLine(cc);
+
+    // division
+    cc = aa / bb;
+    Console.WriteLine(cc);
+    */
