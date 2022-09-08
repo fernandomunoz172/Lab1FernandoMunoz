d9ec2ce777885cbd212d224e94909de5ee80c77e
Fernando Mu?oz <fernando.muoz@snow.edu>
2022-09-08 15:54:34 -0600

    Introduction

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, User my name is Fernando , I will calculate your Generate Point Average. First of all, tell me your name: ");
+name= Console.ReadLine();
